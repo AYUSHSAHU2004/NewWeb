97a07b629200fe8f007ab699cc21b466b33b7bd7
AYUSHSAHU2004 <141218241+AYUSHSAHU2004@users.noreply.github.com>
2024-09-21 16:52:02 +0530

    jfjf

--- Helper/context.js
+++ Helper/context.js
@@ -4,12 +4,12 @@ import { createClient } from "next-sanity";
 import imageUrlBuilder from '@sanity/image-url'
 
 export const client = createClient({
-  projectId: `${process.env.NEXT_PUBLIC_SANITY_STUDIO_PROJECT_ID}`,
+  projectId: '08dz2avh',
 
   dataset: 'production',
   useCdn: false, // set to `false` to bypass the edge cache
   apiVersion: '2024-02-02', // use current date (YYYY-MM-DD) to target the latest API version
-  token: `${process.env.NEXT_PUBLIC_SANITY_STUDIO_SECRET_TOKEN}`,
+  token: 'skgmAktK5smMOrw5Fsf8fP7jm2gnM91CuJ3BjT8Baof22eutvplbgo30PdXmS0uQDjxtw4zY7U3gBBovK8SOUqmq2GQeyqNGTEPrHMxZ8hmyBbTcMQkg8igpZE2VaW53OEyL3pXhBdEjyBK4zcR9DFuPDRQBTA6HDHn4BWiF6dGGE86NuqHl',
 })
 
 export const builder = imageUrlBuilder(client)
